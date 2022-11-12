Addded Jupyter notebook on zero-bias voltage. Fixed normalization of histogram method `plt_utils.py`.

plt_utils.py
@@ -37,7 +37,8 @@ def histogram(values,nbins=100,normalised=False,xlabel=None,ylabel=None,show=Tru
     hist = hist.astype(np.float64)
 
     if normalised:
-        hist /= values.shape[0] #sum of the bin counts will be equal to 1
+        hist /= values.size #sum of the bin counts will be equal to 1
+        print(np.max(hist))
     
     if plt_kwargs: # Note: plt_kwargs is a dictionary of keyword arguments
         if 'color' in plt_kwargs: